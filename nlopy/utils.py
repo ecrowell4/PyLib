d43fde0ecff9530fb7de1ead1c885919ceba3ede
ecrowell4 <ecrowell4@wsu.edu>
2017-11-01 13:36:13 -0700

    Added functionality to nlopy/utils.py.

--- nlopy/utils.py
+++ nlopy/utils.py
@@ -1,4 +1,14 @@
+"""This file containts funtions and classes that should be of general use in Nonlinear optics, regardless
+of application."""
+
 class Units():
+	"""Class whose attributes are the fundamental constants. User can choose from
+	['atomic', 'Gaussian'].
+	Input
+		unit_type : string
+			Specifies the desired type of units
+	"""
+	
 	def __init__(self, unit_type):
 		if unit_type is 'atomic':
 			self.hbar = 1
@@ -7,7 +17,30 @@ class Units():
 			self.c = 137.036
 		elif unit_type is 'Gaussian':
 			self.hbar = 6.58211928e-16 #eV.s
+			self.e = 4.8032042e-10 #esu
 			self.c = 2.99792458e10 #cm/s
-			self.m = 0.51099891e6 / (self.c)**2 #eV.s^2/cm^2 
+			self.m = 0.51099890e6 / (self.c)**2 #eV.s^2/cm^2 
 		else:
 			assert False, "Indicated units not supported. Please choose from ['atomic', Gaussian']."
+
+
+def position_space(L, dx, L_min=0):
+	"""Returns an array that represents a discretized representation of
+	the interval [L_min, L_min + L], enpoints included. 
+
+	Input
+		L : float
+			Length of position space
+		dx : float
+			Grid spacing
+		L_min : float, optional
+			Starting point of grid 
+
+	Output
+		x : np.array
+			Spatial grid
+	"""
+
+	x = np.arange((L + dx) / dx) * dx
+
+	return x
