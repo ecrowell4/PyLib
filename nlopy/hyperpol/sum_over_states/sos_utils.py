b5df7cdadd3993524cf6540463f69e73ea9196c1
ecrowell4 <ecrowell4@wsu.edu>
2017-11-06 12:20:15 -0800

    Edited modified_perturbation to accound for changes in previous commit. Also edited for readability.

--- nlopy/hyperpol/sum_over_states/sos_utils.py
+++ nlopy/hyperpol/sum_over_states/sos_utils.py
@@ -228,8 +228,11 @@ def modified_perturbation(xx_prime, x, psi, psi_prime, E10):
             Modified position matrix
     """
     xx_prime_pert = copy.copy(xx_prime)
+    
     # The transition moment between the quasi degenerate states:
-    xx_prime_pert[1,0] = xx_prime_pert[0,1] = np.trapz(psi_prime[1]*x*psi_prime[0], x) + 0.5*E10*project(x, psi_prime[1], psi[1])*project(x,psi[1], psi_prime[0]) - 0.5*E10*project(x,psi_prime[1], psi[0])*project(x,psi[0], psi_prime[0])
+    xx_prime_pert[1,0] = xx_prime_pert[0,1] = (xx_prime[1,0] 
+    + 0.5*E10*project(x, psi_prime[1], psi[1])*project(x, psi[1], psi_prime[0]) 
+    - 0.5*E10*project(x,psi_prime[1], psi[0])*project(x,psi[0], psi_prime[0]))
     
     return xx_prime_pert
     
