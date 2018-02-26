ee4124923b3cd9700dae0ef91260a3a25f5303d4
Ethan <ecrowell4@gmail.com>
2018-02-25 17:30:54 -0800

    Included method for computing excited state polarizabilities. Also edited PlotFormat usetex=True.

---
+++ nlopy/alpha_exc_st.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 09:56:53 2018
+
+@author: Owner
+"""
+
+import numpy as np
+from nlopy import utils
+from nlopy.quantum_solvers import solver_1D as solve
