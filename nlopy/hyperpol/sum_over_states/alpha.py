e405c88657db02dda6d66d183ba7b4aa840c3c0d
Ethan <ecrowell4@gmail.com>
2018-03-02 12:04:10 -0800

    Added boolean option to alpha.ee(). User can now specify whether alpha or alpha_int is returned.

--- nlopy/hyperpol/sum_over_states/alpha.py
+++ nlopy/hyperpol/sum_over_states/alpha.py
@@ -8,7 +8,7 @@ Created on Mon Jul 31 12:38:46 2017
 import numpy as np
 from nlopy.hyperpol.sum_over_states import sos_utils
 
-def alpha_ee(E, xx, units, omega=0, n=0):
+def alpha_ee(E, xx, units, omega=0, intrinsic=False, n=0):
     """Calculates the polarizability alpha for a system in the ground
     
     Input
@@ -38,7 +38,11 @@ def alpha_ee(E, xx, units, omega=0, n=0):
     alpha =  units.e**2 * (np.delete(xx[n,:], n).dot(np.delete(xx[:,n], n) * sos_utils.D1(np.delete(E, n), omega, units))
                            + np.delete(xx[n,:], n).dot(np.delete(xx[:,n], n) * sos_utils.D1(np.delete(E.conjugate(), n), -omega, units))
                           )
-    return alpha
+
+    if intrinsic is True:
+        return alpha / 2
+    else:
+        return alpha
 
 
 #==============================================================================
