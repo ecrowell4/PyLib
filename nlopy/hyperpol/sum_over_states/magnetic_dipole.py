5378c9e2770864733f7f7caf0cea9348158bbca7
Ethan <ecrowell4@gmail.com>
2019-03-19 09:59:26 -0700

    Deleted SOS_*.py files ad magnetic_dipole.py

--- nlopy/hyperpol/sum_over_states/magnetic_dipole.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from nlopy.hyperpol.sum_over_states import sos_utils
-
-def magnetic_moment_3rd(E, L, I, omega, Efield, Bfield, units):
-    
-    H1 = -units.e * xx * Efield - units.g * L * Bfield 
-    H2 = -(units.e**2 / 2 / units.m / omega[0] / omega[1]) Efield**2
-    
-    
