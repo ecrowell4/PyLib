ed29fa1f2ed3cd9fa186a69d601d278cf02dcb9a
Ethan <ecrowell4@gmail.com>
2019-05-27 11:52:15 -0700

    Created math_utils.py file. Wrote function definitions for functions that will need to be defined, so that I don't forget. Will update this list as I go.

---
+++ nlopy/math_utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numba import jit
+
+@jit(nopython=True)
+def laplacian(y:complex, dx:float)->complex:
+	"""Returns laplacian of complex valued function `y(x)`
+	Dirichlet b.c.s are assumed, implying laplacian vanishes
+	at the boundary."""
+
+@jit(nopython=True)
+def my_convolve(y:complex, h:complex)->complex:
+	"""Returns the convolution of two complex valued functions
+	y*h."""
