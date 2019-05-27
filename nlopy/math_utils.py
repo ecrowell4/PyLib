588cbcc0604d55c16d4da5e4112d72091e30ddd2
Ethan <ecrowell4@gmail.com>
2019-05-27 12:14:29 -0700

    updated statement in math_utils to reflect fact that my_convolve takes three args (f, h, dx) instead of two (f, h).

--- nlopy/math_utils.py
+++ nlopy/math_utils.py
@@ -8,6 +8,6 @@ def laplacian(y:complex, dx:float)->complex:
 	at the boundary."""
 
 @jit(nopython=True)
-def my_convolve(y:complex, h:complex)->complex:
+def my_convolve(y:complex, h:complex, dx:float)->complex:
 	"""Returns the convolution of two complex valued functions
 	y*h."""
