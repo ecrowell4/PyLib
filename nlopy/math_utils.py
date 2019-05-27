628f50b2d91b093f22397ee78035ea3eb6cafdbc
Ethan <ecrowell4@gmail.com>
2019-05-27 12:44:32 -0700

    Defined skeleton of subtact_lagrange() in math_utils.

--- nlopy/math_utils.py
+++ nlopy/math_utils.py
@@ -11,3 +11,21 @@ def laplacian(y:complex, dx:float)->complex:
 def my_convolve(y:complex, h:complex, dx:float)->complex:
 	"""Returns the convolution of two complex valued functions
 	y*h."""
+
+@jit(nopython=True)
+def subtract_lagrange(f:complex, y:complex, dx:float)->complex:
+	"""Subtacts off the set of lagrange multipliers Y from y.
+
+	Input
+	    f : np.array
+	        the variation of some functional F.
+	        In context of HF theory, F is action of HF operator on
+	        som orbital
+	    y : np.array
+	        set of orbitals
+
+	Output
+	    F : np.array
+	        the variation of functional with constraints imposed.
+	"""
+
